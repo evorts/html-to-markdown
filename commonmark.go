fce9ac6d4fd4428ff6f1bbe77295190e0cd3e142
steven <11642329+evorts@users.noreply.github.com>
2020-05-15 13:19:18 +0700

    avoid escape text markdown

--- commonmark.go
+++ commonmark.go
@@ -61,7 +61,7 @@ var commonmark = []Rule{
 			// normal text be indented and thus be a code block.
 			text = multipleSpacesR.ReplaceAllString(text, " ")
 
-			text = escape.MarkdownCharacters(text)
+			//text = escape.MarkdownCharacters(text)
 			return &text
 		},
 	},
